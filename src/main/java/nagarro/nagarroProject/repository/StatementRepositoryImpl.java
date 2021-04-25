b3274cb8bcbbdb235720dd41fd011e2e5779c576
adityaapparaju <83164368+adityaapparaju@users.noreply.github.com>
2021-04-25 19:19:53 +0400

    Update StatementRepositoryImpl.java

--- src/main/java/nagarro/nagarroProject/repository/StatementRepositoryImpl.java
+++ src/main/java/nagarro/nagarroProject/repository/StatementRepositoryImpl.java
@@ -6,9 +6,13 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;  
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 
+import nagarro.nagarroProject.config.SecurityConfiguration;
 import nagarro.nagarroProject.model.Statement;
 
 @Repository
@@ -17,34 +21,49 @@ public class StatementRepositoryImpl implements StatementRepository {
 	@Autowired
     private JdbcTemplate jdbcTemplate;
 	
+	
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 	@SuppressWarnings("deprecation")
 	@Override
 	public List<Statement> findAllByAccountId(Double accountId) {
 		// TODO Auto-generated method stub
 		return  jdbcTemplate.query(
-                "select * from statement where account_id = ?",
+                "select a.account_number as accNumber, "
+                + "s.ID as stId, s.datefield as stDateField, s.account_id as stAccId,"
+                + "s.amount as stAmount from account a join statement s on a.ID = s.account_id  where s.account_id = ?",
                 new Object[]{accountId},
                 (rs, rowNum) ->
                         new Statement(
-                                rs.getInt("id"),
-                                (rs.getDouble("account_id")),
-                                rs.getString("datefield"),
-                                new BigDecimal(rs.getString("amount"))
+                                rs.getInt("stId"),
+                                (rs.getDouble("stAccId")),
+                                rs.getString("stDateField"),
+                                new BigDecimal(rs.getString("stAmount")),
+                                passwordEncoder.encode(rs.getString("accNumber"))
                         )
-        );
+				);
 	}
 
 	@Override
-	public List<Statement> findAll() {
+	public List<Statement> findAll() {             
 		// TODO Auto-generated method stub
 		return jdbcTemplate.query(
-                "select * from statement",
-                (rs, rowNum) ->
+				 "select a.account_number as accNumber, "
+			                + "s.ID as statementId, s.datefield as stDateField, s.account_id as stAccId,"
+			                + "s.amount as stAmount from account a join statement s on a.ID = s.account_id",
+			                (rs, rowNum) ->
                         new Statement(
-                                rs.getInt("id"),
-                                rs.getDouble("account_id"),
-                                rs.getString("datefield"),
-                                new BigDecimal(rs.getString("amount"))
+                                rs.getInt("statementId"),
+                                rs.getDouble("stAccId"),
+                                rs.getString("stDateField"),
+                                new BigDecimal(rs.getString("stAmount")),
+                                passwordEncoder.encode(rs.getString("accNumber"))
                         )
         );
 	}
