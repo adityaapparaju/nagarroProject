9b3c7a23f4ad37c903d3d4bf9995c8955a1520cb
adityaapparaju <83164368+adityaapparaju@users.noreply.github.com>
2021-04-25 19:20:46 +0400

    Update Statement.java

--- src/main/java/nagarro/nagarroProject/model/Statement.java
+++ src/main/java/nagarro/nagarroProject/model/Statement.java
@@ -15,8 +15,17 @@ public class Statement
 
     private BigDecimal amount;
     
+    private String accountNumber;
+    
     
 
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
 
 	public Integer getId() {
 		return id;
@@ -50,12 +59,13 @@ public class Statement
 		this.amount = amount;
 	}
 
-	public Statement(Integer id, Double accountId, String date, BigDecimal amount) {
+	public Statement(Integer id, Double accountId, String date, BigDecimal amount, String accountNumber) {
 		super();
 		this.id = id;
 		this.accountId = accountId;
 		this.dateField = date;
 		this.amount = amount;
+		this.accountNumber = accountNumber;
 	}
 	
 	
